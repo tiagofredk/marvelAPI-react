0bc8e5e363b27f11e8a389a38d868b10fd7dc7f3
Tiago <tiagofredk@gmail.com>
2022-04-18 13:44:16 +0200

    commit

--- src/context/MainContext.js
+++ src/context/MainContext.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState, createContext } from "react";
 import axios from "axios";
-// import api from "../utils/Api";
+import api from "../utils/Api";
 import md5 from "md5";
 export const MainContext = createContext();
 
@@ -11,28 +11,27 @@ export const MainContextProvider = ({ children }) => {
     const [Profile, setProfile] = useState({});
     const [Offset, setOffset] = useState(0)
 
-    useEffect(() => {
-        // async function fetch(){
-        //     try {
-        //         await api.get(`/characters`)
-        //             .then(response => {
-        //                 console.log(response);
-        //                 setCaracters(response.data.data.results);
-        //             })
-        //             .catch(err => console.log(err));
 
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
+    useEffect(() => {
+        // async function fetch() {
+            
+        //     await api.get(`/characters?offset=${Offset}`)
+        //         .then(response => {
+        //             console.log(response);
+        //             setCaracters(response.data.data.results);
+        //         })
+        //         .catch(err => console.log(err));
+
         // }; fetch();
 
         async function fetch() {
+            const baseURL = "http://gateway.marvel.com/v1/public/";
             const publicKey = "a5a4cf7ed033c0ede02a7ebc64639c70";
             const privateKey = "137302d75f04aa04868bbea4642c38dcb9469231";
             const time = Number(new Date());
             const hash = md5(time + privateKey + publicKey);
             try {
-                await axios.get(`http://gateway.marvel.com/v1/public/characters?offset=${Offset}&ts=${time}&apikey=${publicKey}&hash=${hash}`)
+                await axios.get(`${baseURL}/characters?offset=${Offset}&ts=${time}&apikey=${publicKey}&hash=${hash}`)
                     .then(response => {
                         setCaracters(response.data.data.results);
                         console.log(response.data.data.results);
