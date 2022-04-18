ad45062df9f90046996cd7c661a6a4422eb7923c
Tiago <tiagofredk@gmail.com>
2022-04-18 13:44:07 +0200

    commit

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,15 +1,24 @@
+
 import axios from 'axios'
 import md5 from 'md5';
 
+
+// const publicKey = "a5a4cf7ed033c0ede02a7ebc64639c70";
+// const privateKey = "137302d75f04aa04868bbea4642c38dcb9469231";
+
+// const ts = Number(new Date());
+
+// const hash = md5(ts + privateKey + publicKey);
+
 const publicKey = "a5a4cf7ed033c0ede02a7ebc64639c70";
 const privateKey = "137302d75f04aa04868bbea4642c38dcb9469231";
-const ts = Number(new Date());
-const hash = md5(ts + privateKey + publicKey);
+const time = Number(new Date());
+const hash = md5(time + privateKey + publicKey);
 
 const api = axios.create({
     baseURL: "http://gateway.marvel.com/v1/public/",
-    params: {
-        ts,
+    params: {s
+        ts: time,
         apiKey: publicKey,
         hash,
     },
